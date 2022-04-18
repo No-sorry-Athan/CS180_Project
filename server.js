060eb206321dc7746a56e136a1e45bca376615d6
bran-dont <hellobrandontran@gmail.com>
2022-04-18 06:18:20 -0700

    Finishes Search Functionality

--- server.js
+++ server.js
@@ -1,20 +1,59 @@
-const express = require('express');
-const app = express();
+/* eslint-disable semi */
+var express = require('express');
+var exphbs = require('express-handlebars');
+var path = require('path');
+var app = express();
 const port = 3000;
 
 const fs = require('fs');
 const csv = require('csv-parser');
 
-fs.createReadStream('../archive/USVideos.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    app.get('/', (req, res) => res.send(row));
-    // console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
+const { toLower } = require('lodash');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public') , options ));
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'layout'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+var options = { dotfiles: 'ignore', etag: false, extensions: ['htm', 'html'], index: false };
+
+var searchResultsServer = "";
+
+app.get('/', (req, res) => {
+  console.log(searchResultsServer);
+  res.render('home', { title: "YT Analysis", searchResultsClient: searchResultsServer });
+});
+
+app.get('/public/:file', (req, res) => {
+  fs.readFile('./public/' + req.params.file, (error, content) => {
+    if(error) {
+      console.log("Error receiving static file " + req.params.file);
+      throw error;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.end(content, 'utf-8');
   });
+});
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/search', (req, res) => {
+  var query = req.body.YTSearchBar;
+  searchResultsServer = "";
 
-app.listen(port, () => console.log("Test listening on port ${" + port +"}!"));
+  if(query == null) { return; }
+
+  fs.createReadStream('./archive/USVideos.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      if(toLower(row.title).includes(toLower(query))) {
+        searchResultsServer += ('<div>' + row.title + ' / ' + row.trending_date + '</div>');
+      }
+    })
+    .on('end', () => {
+      res.redirect('back');
+    });
+});
+
+app.listen(port, () => console.log('Test listening on port ${' + port + '}!'));
