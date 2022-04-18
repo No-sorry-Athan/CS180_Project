13f0d9b5fadcff498a9d05bafff08da768b6b2a6
Thomas Tran <timtom9801@gmail.com>
2022-04-18 01:43:19 -0700

    Bare template for handlebar

--- server.js
+++ server.js
@@ -2,19 +2,48 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const fs = require('fs');
-const csv = require('csv-parser');
+//loads the handlebars module
+const handlebars = require('express-handlebars');
 
-fs.createReadStream('../archive/USVideos.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    app.get('/', (req, res) => res.send(row));
-    // console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
+//Sets our app to use the handlebars engine
+//app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
-// app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log("Test listening on port ${" + port +"}!"));
+//Sets handlebars configurations
+/*app.engine('handlebars', handlebars ({
+  layoutsDir: __dirname + '/views/layouts',
+})); */
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'planB',
+  //new configuration parameter
+  partialsDir: __dirname + '/views/partials/'
+  }));
+
+//For checking if we have a connection to the server
+// const fs = require('fs');
+// const csv = require('csv-parser');
+
+// fs.createReadStream('../Downloads/USVideos.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     app.get('/', (req, res) => res.send(row));
+//     // console.log(row);
+//   })
+//   .on('end', () => {
+//     console.log('CSV file successfully processed');
+//   });
+
+// // app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.listen(port, () => console.log("Test listening on port ${" + port +"}!"));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //Using the index.hbs file instead of planB
+  res.render('main', {layout: 'index'});});
+
+app.listen(port, () => console.log('App listening to port ${port}'));
