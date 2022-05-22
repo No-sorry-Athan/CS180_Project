a2f0a138a4bd36f32c3562745f521b2868bcc1f9
bran-dont <hellobrandontran@gmail.com>
2022-05-22 12:42:27 -0700

    CSV parser

--- csv_parser.js
+++ csv_parser.js
@@ -93,7 +93,9 @@ function csv_parser2(file) {
 
 function parse(file) {
   const input = fs.readFileSync('./archive/' + file, { encoding: "utf-8" });
+  // console.log(input)
   var rowsArr = input.split('"\r\n');
+  console.log(rowsArr.length);
 
   var quote = [];
   var arr = Array();
@@ -146,9 +148,10 @@ function convert(arr) {
   len = arr.length,
     array = [];
   for (; i < len; i++) {
-    array.push({ "video_id": arr[i][0], "trending_date": arr[i][1], "title": arr[i][2], "channel_title": arr[i][3], "category_id": arr[i][4], "publish_time": arr[i][5], "tags": arr[i][6], "views": arr[i][7], "likes": arr[i][8], "dislikes": arr[i][9], "comment_count": arr[i][10], "thumbnail_link": arr[i][11], "comments_disabled": arr[i][12], "ratings_disabled": arr[i][13], "video_error_or_removed": arr[i][14], "description": arr[i][15] });
+    if(arr[i] != undefined) { console.log(arr[i][0]); console.log(i) }
+    array.push({ "video_id": arr[i][0], "trending_date": arr[i][1], "title": arr[i][2].substring(1, arr[i][2].length - 1), "channel_title": arr[i][3].substring(1, arr[i][3].length - 1), "category_id": arr[i][4], "publish_time": arr[i][5], "tags": arr[i][6].substring(1, arr[i][6].length - 1), "views": arr[i][7], "likes": arr[i][8], "dislikes": arr[i][9], "comment_count": arr[i][10], "thumbnail_link": arr[i][11], "comments_disabled": arr[i][12], "ratings_disabled": arr[i][13], "video_error_or_removed": arr[i][14], "description": arr[i][15].substring(1, arr[i][15].length) });
   }
-  //console.log(array[1].title);
+  console.log(i);
   return array;
 }
 
@@ -156,3 +159,5 @@ module.exports = {
   parse,
   async_parse
 }
+
+parse('USVideos.csv');
